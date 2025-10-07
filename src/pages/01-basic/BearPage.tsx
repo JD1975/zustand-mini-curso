ccba359fbd5a06d7acabbd49dd9a94dddf9d387b
JD1975 <fosa.duarte123@gmail.com>
2025-10-07 13:39:34 -0600

    Add bear management buttons and display in BearPage component

--- src/pages/01-basic/BearPage.tsx
+++ src/pages/01-basic/BearPage.tsx
@@ -1,3 +1,4 @@
+import { WhiteCard } from "../../components";
 import { useBearStore } from "../../stores";
 
 import { WhiteCardBears } from "../../stores";
@@ -6,6 +7,11 @@ export const BearPage = () => {
   const blackBears = useBearStore((state) => state.blackBears);
   const polarBears = useBearStore((state) => state.polarBears);
   const pandaBears = useBearStore((state) => state.pandaBears);
+
+const bears = useBearStore((state) => state.bearsList);
+  const doNothing = useBearStore((state) => state.doNothing);
+  const addBear = useBearStore((state) => state.addBear);
+  const clearBears = useBearStore((state) => state.clearBears);
 
   const increaseBlackBears = useBearStore((state) => state.increaseBlackBears);
   const increasePolarBears = useBearStore((state) => state.increasePolarBears);
@@ -18,7 +24,6 @@ export const BearPage = () => {
       <hr />
 
       <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-2">
-
         <WhiteCardBears
           titleWhiteCard="Osos negros"
           numberOfBears={blackBears}
@@ -36,7 +41,33 @@ export const BearPage = () => {
           numberOfBears={pandaBears}
           changeBears={increasePandaBears}
         />
-        
+
+        <WhiteCard>
+          <button
+            className="bg-blue-500 px-3 py-1 rounded-md text-white"
+            onClick={doNothing}
+          >
+            Do Nothing
+          </button>
+
+          <button
+            className="bg-green-500 px-3 py-1 rounded-md text-white mx-2 mt-2"
+            onClick={addBear}
+          >
+            Add Bear
+          </button>
+
+          <button
+            className="bg-red-500 px-3 py-1 rounded-md text-white mt-2"
+            onClick={clearBears}
+          >
+            Clear Bears
+          </button>
+
+          <pre>
+            {JSON.stringify( bears, null, 2)}
+          </pre>
+        </WhiteCard>
       </div>
     </>
   );
--- src/stores/bears/bears.store.tsx
+++ src/stores/bears/bears.store.tsx
@@ -1,21 +1,52 @@
 import { create } from "zustand";
 
+interface Bear {
+  id: number;
+  name: string;
+}
+
 export interface BearState {
   blackBears: number;
   polarBears: number;
   pandaBears: number;
 
-  increaseBlackBears: (by:number) => void;
-  increasePolarBears: (by:number) => void;
-  increasePandaBears: (by:number) => void;
+  bearsList: Bear[];
+
+  doNothing: () => void;
+  addBear: () => void;
+  clearBears: () => void;
+
+  increaseBlackBears: (by: number) => void;
+  increasePolarBears: (by: number) => void;
+  increasePandaBears: (by: number) => void;
 }
 
 export const useBearStore = create<BearState>()((set) => ({
-    blackBears: 0,
-    polarBears: 0,
-    pandaBears: 0,
+  blackBears: 0,
+  polarBears: 0,
+  pandaBears: 0,
 
-    increaseBlackBears: (by: number) => set((state) => ({ blackBears: state.blackBears + by })),
-    increasePolarBears: (by: number) => set((state) => ({ polarBears: state.polarBears + by })),
-    increasePandaBears: (by: number) => set((state) => ({ pandaBears: state.pandaBears + by })),
+  bearsList: [{ id: 1, name: "Paddington" }],
+
+  doNothing: () => set((state) => ({ bearsList: state.bearsList })),
+
+  addBear: () =>
+    set((state) => ({
+      bearsList: [
+        ...state.bearsList,
+        {
+          id: state.bearsList.length + 1,
+          name: `Oso #${state.bearsList.length + 1}`,
+        },
+      ],
+    })),
+
+  clearBears: () => set({ bearsList: [] }),
+
+  increaseBlackBears: (by: number) =>
+    set((state) => ({ blackBears: state.blackBears + by })),
+  increasePolarBears: (by: number) =>
+    set((state) => ({ polarBears: state.polarBears + by })),
+  increasePandaBears: (by: number) =>
+    set((state) => ({ pandaBears: state.pandaBears + by })),
 }));
