eb43dc95a45f952ebeb1e5314b8b2a2ab7af5971
JD1975 <fosa.duarte123@gmail.com>
2025-10-07 16:15:36 -0600

    Refactor person store: improve code organization and maintainability by separating state and actions

--- src/stores/person/person.store.tsx
+++ src/stores/person/person.store.tsx
@@ -1,20 +1,37 @@
-import { create } from "zustand";
+import { create, StateCreator } from "zustand";
+import { persist } from "zustand/middleware";
 
 interface PersonState {
-    firstName: string;
-    lastName: string;
+  firstName: string;
+  lastName: string;
 }
-
 
 interface PersonActions {
-    setFirstName: (firstName: string) => void;
-    setLastName: (lastName: string) => void;
+  setFirstName: (firstName: string) => void;
+  setLastName: (lastName: string) => void;
 }
 
-export const usePersonStore = create<PersonState & PersonActions> ()((set) => ({
-    firstName: "",
-    lastName: "",
+/* 
+Definición del store
+Separa la definición del estado y las acciones en una constante 'storeApi'
+Esto mejora la legibilidad y el mantenimiento del código 
+*/
 
-    setFirstName: (value: string) => set(state => ({ firstName: value })),
-    setLastName: (value: string) => set(state =>({ lastName: value })),
-}));
+const storeApi: StateCreator<PersonActions & PersonState> = (set) => ({
+  firstName: "",
+  lastName: "",
+
+  setFirstName: (value: string) => set((state) => ({ firstName: value })),
+  setLastName: (value: string) => set((state) => ({ lastName: value })),
+});
+
+/* 
+Persistencia con localStorage
+Se utiliza el middleware 'persist' de Zustand
+La configuración incluye el nombre de la clave en el almacenamiento y es util para
+guardar el estado de la persona entre recargas de página
+*/
+
+export const usePersonStore = create<PersonState & PersonActions>()(
+  persist(storeApi, { name: "personStorage" })
+);
