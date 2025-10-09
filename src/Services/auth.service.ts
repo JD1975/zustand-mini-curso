3fbb51ab2ea470a1158925a84585f4bd07579e84
JD1975 <fosa.duarte123@gmail.com>
2025-10-09 13:25:00 -0600

    Add request interceptor to tesloApi for token authorization and enhance AuthService with error handling for checkStatus method

--- src/Services/auth.service.ts
+++ src/Services/auth.service.ts
@@ -11,6 +11,12 @@ export interface LoginResponse {
 }
 
 export class AuthService {
+
+  // * Metodo para hacer login
+  // Ahora retorna toda la información del usuario junto con el token
+  // Esto permite actualizar el estado de AuthState en el store
+  // con toda la información necesaria tras el login
+  
   static login = async (email: string, password: string):Promise<LoginResponse> => {
     try {
       const {data} = await tesloApi.post<LoginResponse>("/auth/login", {
@@ -26,4 +32,25 @@ export class AuthService {
       throw new Error("Error al iniciar sesión");
     }
   };
+
+  // * Metodo para validar token/Status
+  static checkStatus = async (token: string):Promise<LoginResponse> => {
+    try {
+
+      const {data} = await tesloApi.get<LoginResponse>("/auth/check-status", {
+        headers: {
+          Authorization: `Bearer ${token}`,
+        },
+      });
+      return data;
+
+    }catch (error) {
+      throw new Error("Unathorized");
+
+    }
+
+
+
+  }
+
 }
